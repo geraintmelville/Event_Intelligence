63ef2d32974b6d9ca8a132f9cf52791bbb4134a5
geraintmelville <geraint.melville@gmail.com>
2026-07-11 12:26:24 +0100

    App update

--- insights.py
+++ insights.py
@@ -8,4 +8,16 @@ def create_top_20_cities(data):
   top_20_cities = top_20_cities.drop(index='London') # drop London from data as it is an outlier
   top_20_cities = pd.DataFrame(top_20_cities).reset_index().rename(columns={'event_id': 'nb_events'})
   
+<<<<<<< Updated upstream
   return london, non_london_events, top_20_cities
+=======
+  return london, top_20_cities
+
+def create_top_20_areas(data):
+  top_20_areas = data.groupby('area')['event_id'].count().sort_values(ascending=False).head(21)
+  westminster = top_20_areas['Westminster']
+  top_20_areas = top_20_areas.drop(index='Westminster') # drop London from data as it is an outlier
+  top_20_areas = pd.DataFrame(top_20_areas).reset_index().rename(columns={'event_id': 'nb_events'})
+  
+  return westminster, top_20_areas
+>>>>>>> Stashed changes
